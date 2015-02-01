ca9a39527db5efead32fa1f95313eca6eda7b5a9
Derek <derekr@me.com>
2015-02-01 13:03:02 -0800

    Adds tap to view experience
    - Includes lightbox transitions
    - Adds currentIndex and photoSet to env for easy app state access

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,8 +1,27 @@
-var PHOTO_PATH = 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=efc8796c7f772d7c7855d7e88caf0e41&photoset_id=72157626579923453&extras=url_m%2C+url_l&per_page=9&format=json&nojsoncallback=1&api_sig=d02684deb150e314c092a5666a7727d4';
+/**
+ * Environment variable object
+ */
+var env = {
+    FLICKR_KEY: '47cd2133406a258aa24cf785547ffac5',
+    FLICKR_HOST: 'https://api.flickr.com',
 
+    PHOTOSET: '72157626579923453',
+    PER_PAGE: 9,
+
+    currentIndex: 0,
+    photoSet: []
+};
+
+/**
+ * Normalize response in to standard object incase I want to mess
+ * around with other APIs.
+ *
+ * @param {object} res – XMLHttpRequest response object.
+ */
 function mapResponse (res) {
-    return res.photoset.photo.map(function (photo) {
+    return res.photoset.photo.map(function (photo, i) {
         return {
+            index: i,
             title: photo.title,
             lrg: {
                 url: photo.url_l,
@@ -18,29 +37,63 @@ function mapResponse (res) {
     });
 }
 
+var PHOTO_API_PATH =
+    env.FLICKR_HOST +
+    '/services/rest/?method=flickr.photosets.getPhotos&' +
+    'api_key=' + env.FLICKR_KEY + '&' +
+    'photoset_id=' + env.PHOTOSET + '&extras=url_m%2C+url_l&' +
+    'per_page=' + env.PER_PAGE + '&format=json&nojsoncallback=1&';
+
+/**
+ * Async request to photo API.
+ *
+ * @prop {function} callback - Called w/ err, photos.
+ */
 function fetchPhotos (callback) {
     var req = new XMLHttpRequest();
 
+    function onResponse (req) {
+        if (req.status >= 200 && req.status < 400) {
+            callback(
+                null,
+                mapResponse(JSON.parse(req.responseText))
+            );
+        } else {
+            callback(req.status);
+        }
+    }
+
     // Feature detection for CORS
     if ('withCredentials' in req) {
-        req.open('GET', PHOTO_PATH, true);
+        req.open('GET', PHOTO_API_PATH, true);
         // Just like regular ol' XHR
         req.onreadystatechange = function() {
-            if (req.readyState === 4) {
-                if (req.status >= 200 && req.status < 400) {
-                    callback(
-                        null,
-                        mapResponse(JSON.parse(req.responseText))
-                    );
-                } else {
-                    callback(req.status);
-                }
-            }
+            if (req.readyState === 4) onResponse(req);
         };
+
+        req.onerror = onResponse; // error handled in onResponse
+
         req.send();
     }
 }
 
+function loadImg (src, callback) {
+    callback = (typeof callback === 'function') ? callback : function () {};
+
+    var el = new Image();
+
+    el.onload = function () { callback(null, el); }
+    el.onerror = function (err) { callback(err, el); }
+
+    el.src = src;
+}
+
+/**
+ * Render list of photos. Each photo object is mapped to
+ * `li` elements.
+ *
+ * @param {array} photos - Array of photos from `fetchPhotots`.
+ */
 function listHtml (photos) {
     var container = document.createElement('div');
     var li = document.createElement('li');
@@ -52,16 +105,86 @@ function listHtml (photos) {
     anchor.appendChild(img);
 
     return photos.map(function (p) {
+        li.dataset.index = p.index;
         anchor.href = p.lrg.url;
         img.src = p.med.url;
         return container.innerHTML;
     }).join('\n');
 }
 
+/**
+ * Initializes lightbox with provided img.
+ *
+ * @param {object} img - Image object to initialize with.
+ */
+var lightbox = (function lightbox () {
+    var ACTIVE_CLASS = 'is-active';
+    var LOADING_CLASS = 'is-loading';
 
+    var el = document.getElementById('lightbox');
+    var activeImg = document.getElementById('active-photo-img');
+    var activeTitle = document.getElementById('active-photo-title');
+
+    el.addEventListener('click', function (e) {
+        if (e.target.classList.contains('js-close-lightbox')) {
+            el.classList.remove(ACTIVE_CLASS);
+            setTimeout(function () {
+                activeImg.src = '/images/blank.gif'; // prevent invalid img border
+                activeTitle.innerText = '';
+                el.classList.remove(LOADING_CLASS);
+            }, 80);
+        }
+    });
+
+    return function (img) {
+        env.currentIndex = img.index;
+
+        loadImg(img.lrg.url, function (err, _img) {
+            activeTitle.innerText = img.title;
+            activeImg.src = _img.src;
+
+            setTimeout(function () {
+                el.classList.remove(LOADING_CLASS);
+            }, 80);
+        });
+
+        el.classList.add(LOADING_CLASS, ACTIVE_CLASS);
+    }
+})();
+
+/**
+ * Photo list view that handles img selection.
+ *
+ * @param {object} el - View root element. Should contain `ul.photo-list`.
+ * @param {object} opts - View options.
+ *   - onSelect: Callback triggered when an image is selected. A img object is
+ *               the only argument passed in.
+ */
+function photoList (el, opts) {
+    var list = document.getElementById('photo-list');
+    list.innerHTML = listHtml(opts.photos);
+
+    el.addEventListener('click', function (e) {
+        var index = e.target.parentNode.parentNode.dataset.index;
+
+        if (typeof opts.onSelect === 'function') {
+            opts.onSelect(opts.photos[index]);
+        }
+
+        e.preventDefault();
+    }, true);
+}
+
+/**
+ * Blastoff
+ */
 fetchPhotos(function (err, photos) {
     if (err) console.error('Error fetching photos.');
 
-    var list = document.getElementById('photo-list');
-    list.innerHTML = listHtml(photos);
+    env.photoSet = photos;
+
+    photoList(document.getElementById('photos'), {
+        photos: photos,
+        onSelect: function (img) { lightbox(img); }
+    });
 });
